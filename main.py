06be5e87aa5ee1a79310cc31579a11d9cea84320
Yoon Min <minusxix@gmail.com>
2024-03-22 12:54:34 +0700

    'export'

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ load_dotenv()
 api_key = os.getenv('SERPAPI_KEY')
 client = serpapi.Client(api_key=api_key)
 
-search = 'sắt'
+search = 'cửa hàng'
 
 results = client.search({
     'engine': 'google_maps',
@@ -31,9 +31,9 @@ print(results) #python main.py
 with open('output.csv', 'w', encoding='utf-8', newline='') as csv_file:
     csv_writer = csv.writer(csv_file)
 
-    csv_writer.writerow(["Title", "Address", "Phone"])
+    csv_writer.writerow(['Title', 'Address', 'Phone'])
 
     for result in local_results:
-        csv_writer.writerow([result["title"], result["address"] if "address" in result else "", result["phone"] if "phone" in result else ""])
+        csv_writer.writerow([result['title'], result['address'] if 'address' in result else '', result['phone'] if 'phone' in result else ''])
 
 print('Done')
